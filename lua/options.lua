ebcf002f127f07608c08d969834d9fde2ae48c66
coderj001 <amirajubolchi001@gmail.com>
2021-03-30 08:58:35 +0530

    set global for nvim

--- lua/options.lua
+++ lua/options.lua
@@ -22,6 +22,33 @@ local function load_options()
         wildmode = 'longest,list,full';
         wildmenu = true;
         wildignore = '.git,.hg,.svn,*.aux,*.out,*.toc,*.o,*.obj,*.exe,*.dll,*.manifest,*.rbc,*.class,*.ai,*.bmp,*.gif,*.ico,*.jpg,*.jpeg,*.png,*.psd,*.webp,*.avi,*.divx,*.mp4,*.webm,*.mov,*.m2ts,*.mkv,*.vob,*.mpg,*.mpeg,*.mp3,*.oga,*.ogg,*.wav,*.flac,*.eot,*.otf,*.ttf,*.woff,*.doc,*.pdf,*.cbr,*.cbz,*.zip,*.tar.gz,*.tar.bz2,*.rar,*.tar.xz,*.kgb,*.swp,.lock,.DS_Store,._*';
+        title = true;
+        conceallevel = 2;
+        splitright = true;
+        splitbelow = true;
+        ruler = true;
+        hid = true;
+        cmdheight = 2;
+        ignorecase = true;
+        hlsearch = true;
+        smartcase = true;
+        incsearch = true;
+        lazyredraw = true;
+        showmatch = true;
+        backspace='eol,start,indent';
+        mat = 2;
+        foldcolumn = 1;
+        foldmethod = 'indent';
+        foldnestmax = 10;
+        nofoldenable = true;
+        foldlevel = 2;
+        exrc = true;
+        encoding = 'utf-8';
+        ffs = 'unix,dos,mac';
+        nobackup = true;
+        nowb = true;
+        noswapfile = true;
+        updatetime = 300;
     }
     bind_option(global_local)
 end
